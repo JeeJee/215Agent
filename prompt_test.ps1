781edb4af863ca9788667f10954b81443399ff7f
JeeJee <raducristiandinca@gmail.com>
2025-05-25 15:01:16 +0300

    new phone who this?

---
+++ prompt_test.ps1
@@ -0,0 +1,19 @@
+# Prompt user for input
+$userPrompt = Read-Host "Enter your prompt"
+
+# Create JSON body
+$body = @{
+    model = "mistral"
+    prompt = $userPrompt
+    stream = $false
+} | ConvertTo-Json -Depth 3
+
+# Send POST request
+$response = Invoke-RestMethod `
+    -Uri "http://localhost:11434/api/generate" `
+    -Method Post `
+    -Body $body `
+    -ContentType "application/json"
+
+# Output response
+$response
